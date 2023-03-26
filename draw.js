41880d72cbb53db7ee985109d26e21ffc95c78a0
Aditya Kumar <aditya201292@gmail.com>
2023-03-26 18:47:35 +0530

    Add size button

--- draw.js
+++ draw.js
@@ -1,12 +1,30 @@
 const container = document.querySelector('.container');
+const size = document.querySelector('.size');
+let gridItems = 0;
+createGrid(16);
 
-for(let i=0;i<256;i++){
-    const grid = document.createElement('div');
-    grid.classList.add('grid-items');
-    container.appendChild(grid);
-}
+size.addEventListener('click',()=>{
+    let gSize = prompt("Enter number of squares/side.(limit 1-100)");
+    if(gSize>0 && gSize<=100){
+        gridItems.forEach(item => container.removeChild(item));
+    }
+    createGrid(gSize);
 
-const gridItems = document.querySelectorAll('.grid-items');
-gridItems.forEach(item => item.addEventListener('mouseenter',()=>{
-    item.style.cssText = 'background:black;';
-}) )
+});
+
+function createGrid(n){
+    for(let i=0;i<(n*n);i++){
+        const grid = document.createElement('div');
+        grid.classList.add('grid-items');
+        grid.style.cssText = 'flex-grow:1; flex-shrink:1';
+        let height = parseInt(600/(n)); 
+        grid.style.flexBasis = `${height}px`;   
+        container.appendChild(grid);
+    }
+    gridItems = document.querySelectorAll('.grid-items');
+    gridItems.forEach(item => item.addEventListener('mouseenter',()=>{
+    item.classList.add('blackBack');
+    }) );
+
+};
+
